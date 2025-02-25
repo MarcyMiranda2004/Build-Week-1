2030af6ebc37fbcf014c7349f093220f5313945a
Roberto Marino <rm.marinoroberto@gmail.com>
2025-02-25 11:20:23 +0100

    upgrade css and html

---
+++ assets/js/resultspage.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const ctx = document.getElementById("resultChart").getContext("2d");
+
+  new Chart(ctx, {
+    type: "doughnut",
+    data: {
+      datasets: [
+        {
+          data: [66.7, 33.3],
+          backgroundColor: ["#00eaff", "#b033a3"],
+          borderWidth: 0,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      cutout: "75%",
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+});
